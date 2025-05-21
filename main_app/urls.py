a35f4ca0df0a8bc02b4192a63fb32cf9f9f26172
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2025-05-21 15:37:16 +0000

    template ntegration

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
+    path("sign_in/", views.sign_in, name="login"),
+    path("sign_up/", views.sign_up, name="sign_up"),
+    path("forgot_password/", views.forgot_password, name="forgot_password"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html")
+
+def sign_in(request):
+    return render(request, "sign_in.html")
+
+def sign_up(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password1']
+        User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
+    return render(request, "sign_up.html")
+
+def forgot_password(request):
+    return render(request, "forgot_password.html")
